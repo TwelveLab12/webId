8fbaaf2bfa0f29f3bd60bb63ad2059dbc8e25c1f
TwelveLab12 <bruno.schvartz.pro@gmail.com>
2018-09-30 19:46:13 +0200

    create wishfile index view

---
+++ resources/views/whishfiles/index.blade.php
@@ -0,0 +1,39 @@
+@extends('layouts.app')
+
+@section('content')
+
+    @if(Session::has('errors'))
+        {{ Session::get('errors')}}
+    @endif
+
+
+    <form action="{{route('whishfiles.store')}}" method="POST" enctype="multipart/form-data"  >
+
+        @csrf
+
+        <input type="file" name="file"  @change="majfilename"/>
+        <button type="submit" name="button">
+            Save file
+        </button>
+
+    </form>
+
+    <div  class="row">
+        <div class="col-12">
+            @{{filename}}
+        </div>
+    </div>
+    @foreach($files as $file)
+
+        <div class="row">
+            <div class="col-6">
+                {{ $file->name }}
+            </div>
+            <div class="col-6">
+                {{ $fileCfg['path'] . '/' . $file->type . '/' . $file->pathname . '.' .$file->extension }}
+            </div>
+        </div>
+
+    @endforeach
+
+@endsection
