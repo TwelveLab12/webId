2f1c4b432e74a84baf3b24685dc1509bc825d6fc
TwelveLab12 <bruno.schvartz.pro@gmail.com>
2018-10-14 12:41:29 +0200

    add nav items in config folders
    each item should has name and label
    - name is route name in route file
    - label is the translation path

---
+++ config/navbar.php
@@ -0,0 +1,14 @@
+<?php
+
+return [
+        'items' => [
+            [
+                'name' => 'wishfiles.create',
+                'label' => 'app.navbar.wishfiles.create'
+            ],
+            [
+                'name' => 'wishfiles.list',
+                'label' => 'app.navbar.wishfiles.list'
+            ],
+        ],
+];
