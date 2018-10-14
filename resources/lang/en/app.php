2939eefc1711d72bd420ae9226e2ff38ea9c4db6
TwelveLab12 <bruno.schvartz.pro@gmail.com>
2018-10-14 11:14:20 +0200

    add en translations

---
+++ resources/lang/en/app.php
@@ -0,0 +1,15 @@
+<?php
+
+return [
+    'head' => [
+        'title' => 'wishfiles'
+    ],
+    'main_title' => 'asked for documents simply',
+    'navbar' => [
+        'home' => 'home',
+        'wishfiles' => [
+            'create' => 'create',
+            'list' => 'list',
+        ]
+    ]
+];
